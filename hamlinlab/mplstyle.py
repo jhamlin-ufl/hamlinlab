0449be378f429d871b80ce75dc9a05b4ea163a38
jhamlin-ufl <jhamlin@ufl.edu>
2023-02-23 15:17:15 -0500

    Flattened module structure.

--- hamlinlab/mplstyle.py
+++ hamlinlab/mplstyle.py
@@ -4,24 +4,33 @@ Here are references to commonly used matplotlib styles.
 
 import pathlib
 
+__all__ = [
+    'prb_regular',
+    'prb_wide',
+    'prb_verywide',
+    'prb_tall'
+    ]
+
 cwd = pathlib.Path.cwd() # The current working directory
-project_root = cwd.parent
 
 # Here we specify the folder where our custom styles live
-mpl_config_dir = project_root / 'matplotlib_styles'
+mpl_config_dir = cwd / 'matplotlib_styles'
 
 prb_regular = (
     mpl_config_dir / 'prb_defaults.mplstyle',
     mpl_config_dir / 'prb_regular.mplstyle'
     )
+
 prb_wide = (
     mpl_config_dir / 'prb_defaults.mplstyle',
     mpl_config_dir / 'prb_wide.mplstyle'
     )
+
 prb_verywide = (
     mpl_config_dir / 'prb_defaults.mplstyle',
     mpl_config_dir / 'prb_verywide.mplstyle'
     )
+
 prb_tall = (
     mpl_config_dir / 'prb_defaults.mplstyle',
     mpl_config_dir / 'prb_tall.mplstyle'
